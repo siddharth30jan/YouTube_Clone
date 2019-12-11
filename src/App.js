a698428242319f4194dfb7c7e7d947d9c57e0608
Siddharth Singh <siddharths51700@gmail.com>
2019-12-11 12:24:53 +0530

    Completed the API calling feature usinf fetch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Nav from './components/Nav'
 
 function App() {
+  const [videoList,fvideoList]=useState([])
   return (
-  <Nav />
+  <Nav videoList={videoList} fvideoList={fvideoList}/>
   );
 }
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,27 +1,44 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 const style = {
   display: "flex",
   justifyContent: "space-around",
   background: "grey"
 };
-const Nav = () => {
-  const [result,fResult]=useState()
-  const myFunction=(e)=>{
-
+const Nav = ({ videoList, fvideoList }) => {
+  const [result, fResult] = useState();
+  const myFunction = async e => {
     //API calling
-    console.log(result)
-  }
+    try {
+      let response = await fetch(
+        `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&key=AIzaSyB-9jZwJX4l3i68xcfXN9SFB5eQjxnVJgc&q=${result}`
+      );
+      let data = await response.json();
+     // console.log(data.items);
+     fvideoList(data.items)
+    } catch (err) {
+      console.log(err);
+    }
+
+    console.log(result);
+  };
   return (
     <div style={style}>
-      <h1 style={{marginTop: "0px",color: "white"}}>YouTube</h1>
-      <div >
+      <h1 style={{ marginTop: "0px", color: "white" }}>YouTube</h1>
+      <div>
         <input
           type="text"
           placeholder="Search"
           style={{ width: "800px", height: "30px" }}
-          onChange={(e)=>{fResult(e.target.value)}}
+          onChange={e => {
+            fResult(e.target.value);
+          }}
         />
-        <button style={{height: "30px",cursor: "pointer" }} onClick={myFunction} >Search</button>
+        <button
+          style={{ height: "30px", cursor: "pointer" }}
+          onClick={myFunction}
+        >
+          Search
+        </button>
       </div>
     </div>
   );
